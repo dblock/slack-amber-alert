a586ca7aab509cbb0754df60a3bca71cbb33d605
dblock <dblock@dblock.org>
2016-05-31 06:54:32 -0400

    Removed help GIF.

--- slack-bot-server/commands/help.rb
+++ slack-bot-server/commands/help.rb
@@ -15,7 +15,6 @@ help      - get this helpful message
 EOS
       def self.call(client, data, _match)
         client.say(channel: data.channel, text: [HELP, SlackBotServer::INFO].join("\n"))
-        client.say(channel: data.channel, gif: 'help')
         logger.info "HELP: #{client.owner}, user=#{data.user}"
       end
     end
